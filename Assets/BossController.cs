cfe3e402d9be2c3371b2c0b331c72daf04da48ba
J.P.Barnes U1357474 <U1357474@unimail.hud.ac.uk>
2016-10-20 17:05:46 +0100

    Started the boss script

---
+++ Assets/BossController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossController : MonoBehaviour {
+
+    public float base_health = 5.0f;
+    public float attack_stength = 2.0f;
+    
+    public int base_shield_amount = 100;
+    public int shield_multipler = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+
+    }
+}
