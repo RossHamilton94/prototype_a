1cebf48e3436d086986b6738cc053178f4bc36cd
R.Hamilton U1358389 <U1358389@unimail.hud.ac.uk>
2016-10-20 16:07:46 +0100

    Seperated input from Player into PlayerInput and DroneController to prepare for AI drones. Player script still present just in case

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    private float move_x = 0.0f;
+    private DroneController dc;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        dc = GetComponent<DroneController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moving
+        move_x = Input.GetAxis("Horizontal");
+        dc.Move(move_x);
+
+        //jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dc.Jump();
+        }
+
+        // shooting
+        if (Input.GetButtonDown("Fire1"))
+        {
+            dc.FireWeapon();
+        }
+    }
+}
