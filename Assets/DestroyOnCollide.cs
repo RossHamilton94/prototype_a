5cc5de0c66bf3e71d3318675115d868d165d1f32
J.P.Barnes U1357474 <U1357474@unimail.hud.ac.uk>
2016-10-20 16:29:32 +0100

    Added some stuff, fixed rockets

--- Assets/DestroyOnCollide.cs
+++ Assets/DestroyOnCollide.cs
@@ -7,20 +7,14 @@ public class DestroyOnCollide : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
 		Destroy (this.gameObject, lifetime);
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
-	
 	}
 
 	void OnCollisionEnter(Collision col) {
-	
 		if (col.transform.tag == "Wall") {
 			Destroy(this.gameObject);
 		}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public float speed = 8.5f;
     public float jump_strength = 5.0f;
@@ -21,8 +23,8 @@ public class Player : MonoBehaviour {
     private GameObject bullet;
 
     // Use this for initialization
-    void Start() {
-
+    void Start()
+    {
         // setup player objects
         rb = GetComponent<Rigidbody>();
         // Camera.main.transform.GetComponent<SmoothFollow>().target = this.transform;
@@ -33,62 +35,117 @@ public class Player : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
         // moving
         move_x = Input.GetAxis("Horizontal");
 
+        if (move_x > 0)
+        {
+            facing = 1;
+        }
+        else if (move_x < 0)
+        {
+            facing = -1;
+        }
+        else if (move_x == 0)
+        {
+            if (facing == 1)
+            {
+                facing = 1;
+            }
+            else if (facing == -1)
+            {
+                facing = -1;
+            }
+        }
+
+        // facing = move_x > 0 ? 1 : -1;
+
         // jumping
-        if (is_jumping && !double_jumped) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                if (rb.velocity.y < 0) {
+        if (is_jumping && !double_jumped)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (rb.velocity.y < 0)
+                {
                     double_jumped = true;
                     Jump();
                 }
             }
-        } else if (!is_jumping) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
+        }
+        else if (!is_jumping)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 is_jumping = true;
                 Jump();
             }
         }
-        if (rb.velocity.magnitude <= float.Epsilon) {
+        if (rb.velocity.magnitude <= float.Epsilon)
+        {
             is_jumping = false;
             double_jumped = false;
         }
 
         // shooting
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButtonDown("Fire1"))
+        {
             GameObject bullet_fired = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+            bullet_fired.transform.rotation = Quaternion.Euler(new Vector3(0.0f, facing == 1 ? 0.0f : 180.0f, 0.0f));
+            bullet_fired.SetActive(true);
             bullet_fired.GetComponent<Rigidbody>().AddForce(new Vector2(bullet_force * 1.0f * facing, 0.0f), ForceMode.Impulse);
         }
 
     }
 
-    void Jump() {
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(new Vector2(20.0f, 20.0f), new Vector2(150.0f, 150.0f)),
+            "Facing: " + (facing == 1 ? "Right" : "Left") + "\n" +
+            "Last Facing: " + (facing == 1 ? "Right" : "Left") + "\n" +
+            "Is Jumping: " + (is_jumping == true ? "Yes" : "No") + "\n"
+           );
+    }
+
+    void Jump()
+    {
         rb.velocity = new Vector2(rb.velocity.x, 0.0f);
         rb.AddForce(new Vector2(0, 1 * jump_strength), ForceMode.Impulse);
     }
 
-    void FixedUpdate() {
-		transform.Translate(new Vector3(0.0f, 0.0f, speed * -move_x * Time.deltaTime));
-        facing = move_x > 0 ? 1 : -1;
+    void FixedUpdate()
+    {
+        rb.transform.Translate(new Vector3(0.0f, 0.0f, speed * ((facing == 1) ? -move_x : move_x) * Time.deltaTime));
+        if (move_x != 0)
+        {
+            this.transform.rotation = Quaternion.Euler(new Vector3(0.0f, facing == -1 ? 90.0f : 270.0f, 0.0f));
+        }
     }
 
-    void AddHealth(int amount) {
+    void AddHealth(int amount)
+    {
 
-        if (health + amount <= 0) {
+        if (health + amount <= 0)
+        {
             Debug.Log("You're dead.");
-        } else {
+        }
+        else
+        {
             health += amount;
         }
     }
 
-    void AddAmmo(int amount) {
+    void AddAmmo(int amount)
+    {
 
-        if (ammo + amount <= 0) {
+        if (ammo + amount <= 0)
+        {
             Debug.Log("You're out of ammo, please reload.");
-        } else {
+        }
+        else
+        {
             ammo += amount;
         }
     }
